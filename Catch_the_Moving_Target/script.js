2c1d37dcefb7cfb99e3b94d752e10a4f7985f1f8
Rumit Varsani <147755424+Rumit-Varsani@users.noreply.github.com>
2025-02-11 23:43:44 +0000

    13th project created

---
+++ Catch_the_Moving_Target/script.js
@@ -0,0 +1,6 @@
+function movetotargetrandomly()
+{
+    const target = document.querySelector(".target");
+    const randomx = Math.random * (window.innerWidth-50);
+    const randomy = Math.random * (window.innerHeight-50);
+}
